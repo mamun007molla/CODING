be3f6be5f977fa07b117b77ce611f4cf00a5347d
Mamun <majanfermun@gmail.com>
2025-07-11 10:23:44 +0600

    added python file

---
+++ 100DAYS -PYTHON/Day 8 - Function Parameters & Caesar Cipher/function_with_input.py
@@ -0,0 +1,105 @@
+# function creation and called
+'''
+def functionName():
+    // do this
+    // then do this
+    // finally do this
+
+functionName()
+'''
+
+'''
+1. create a function called greet()
+2. write 3 print statement inside the function
+3. call the greet() and run your code
+
+'''
+
+# def greet():
+#     print("Hello Mamun")
+#     print("How do you do Jack?")
+#     print("Isn't the weather nice today?")
+# greet()
+
+
+
+
+
+
+# function with inputs as positional argument
+
+'''
+def functionName(something as parameter):
+    // do this wtih something
+    // then do this
+    // finally do this
+
+functionName(something as argument)
+
+'''
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name} ?")
+
+# greet_with_name("Mamun")
+
+
+
+
+
+
+
+
+# function with multiple inputs positional arguments
+'''
+def functionName(param1,param2,..paramn):
+    // do this wtih something
+    // then do this
+    // finally do this
+
+functionName(aug1,aug2,..augn)
+
+here param1=aug1, param2=aug2 and paramn=augn
+
+'''
+
+# def greet_with(name,location):
+#     print(f"Hello {name}\nWhat is it like in {location}")
+
+# greet_with("MAMUN","KUDDUSNAGAR,KONABARI,GAZIPUR")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# function with multiple inputs keyword arguments
+'''
+def functionName(param1,param2,..paramn):
+    // do this wtih something
+    // then do this
+    // finally do this
+
+functionName(param1=aug1,param2=aug2,..paramn=augn)
+functionName(param2=aug2,param1=aug1,..paramn=augn)
+
+
+
+'''
+
+def greet_with(name,location):
+    print(f"Hello {name}\nWhat is it like in {location}")
+
+greet_with(name="MAMUN",location="KUDDUSNAGAR,KONABARI,GAZIPUR")
+greet_with(location="KUDDUSNAGAR,KONABARI,GAZIPUR",name="MAMUN")
+
+
+
