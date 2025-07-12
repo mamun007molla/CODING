56da41de07f255f6ce879720812c3469e743b663
Mamun <majanfermun@gmail.com>
2025-07-12 10:56:49 +0600

    caeser cipher finished

--- 100DAYS -PYTHON/Day 8 - Function Parameters & Caesar Cipher/caeserr-cipher_reorganise.py
+++ 100DAYS -PYTHON/Day 8 - Function Parameters & Caesar Cipher/caeserr-cipher_reorganise.py
@@ -4,9 +4,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+ 
     #e.g. 
     #plain_text = "hello"
     #shift = 5
@@ -18,13 +16,18 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-def encrypt(text,shift):
+ 
+def caeser(text,shift,direction):
     new_text=""
+    alp=-25
+    if direction=="decode":
+            shift*=-1
+            alp*=-1
     for i in text:
+       
         if i in alphabet:
-            if ord(i)+shift>122:
-                count=ord(i)+1+shift-26
+            if (ord(i)+shift>122) or (ord(i)+shift<97 ):
+                count=ord(i)+alp+shift
                 
                 new_text+=chr(count)
             else:
@@ -32,37 +35,28 @@ def encrypt(text,shift):
         else:
             new_text+=i
     
-    print(f"Here's the encoded result: {new_text}")
-
-def decode(text,shift):
-    new_text=""
-    for i in text:
-        if i in alphabet:
-            if ord(i)-shift<97:
-                count=ord(i)-shift+26-1
-                
-                new_text+=chr(count)
-            else:
-                new_text+=chr(ord(i)-shift)
-        else:
-            new_text+=i
-    
-    print(f"Here's the decoded result: {new_text}")
+    print(f"Here's the {direction}d result: {new_text}")
 
 while True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction !="encode":
+        if direction!="decode":
+            print("it is wrong input.please press the right value")
+            continue
 
     
 
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    if direction=="encode":
-        encrypt(text,shift)
-    elif direction=="decode":
-        decode(text,shift)
+    if type(shift)!= int:
+        print("please give me int value only")
+        continue
+    shift=shift%26
+    caeser(text,shift,direction)
     
     next=input("Type 'yes' if you want to go again. otherwise type 'no'.")
     if next=="no":
+        print("Good Bye")
         break
         
 
